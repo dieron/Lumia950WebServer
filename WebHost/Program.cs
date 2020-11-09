04b4d9d4bcd8ba55a5a26cabae2da905035f8d6e
vladimir_mvsc <vladimir@dmvdesk.com>
2020-11-09 19:46:47 +0200

    Initial commit with basic site structure made from a template

---
+++ WebHost/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace MDGriphe.Experiments.Lumia950.WebHost
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
